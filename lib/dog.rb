ac298e4637398f60221c76ecf8dfa2cbe072757d
Wyatt Goodwin <wog6@cornell.edu>
2019-09-09 19:13:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+# Your codegoes here!
+class Dog
+  def initialize(name = nil)
+    @name = name
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
